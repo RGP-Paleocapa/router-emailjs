b551b46e89651250aec3144dcdc87bc0f19e5578
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2025-07-09 12:44:09 +0200

    some changes

---
+++ app/pages/home/Home.tsx
@@ -0,0 +1,5 @@
+export const Home = () => (
+    <main className="flex items-center justify-center pt-16 pb-4">
+        Hello
+    </main>
+);
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Home as Welcome } from "../pages/home/Home";
 
 export function meta({}: Route.MetaArgs) {
   return [
